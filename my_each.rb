22bb0683c60fdbd72a7775e2241fe2e64caf18c3
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-04-04 02:41:44 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
+def my_each(words) # put argument(s) here
   # code here
+  i = 0
+  while i < words.length
+    yield words[i]
+    i = i + 1
+    words << words[i]
+  end
 end
+
+cobber = ["meow", "yay", "dick"]
+my_each(cobber){|i|}
